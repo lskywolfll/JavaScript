2d322bec7852161785531e1e50a90d32af4ae636
René <renealejandrosanchezmorales@gmail.com>
2019-08-13 13:12:42 -0400

    SEe añadio explicacion en ciclos en escuela de javascript

--- Escuela de JavaScript/ciclos.js
+++ Escuela de JavaScript/ciclos.js
@@ -33,12 +33,16 @@ const decrementoDePeso = (persona) => persona.peso -= INCREMENTO_PESO;
 // }
 // console.log(`Al final del año ${rene.nombre} pesa ${rene.peso.toFixed(1)}kg`)
 
+// Meta que queremos bajar de peso en este caso 3kg
 const META = rene.peso - 3;
+// Contador de los dias en los cuales nosotros iremos contando para ver cuanto nos demoramos en adelgazar
 var cantidadDeDias = 0;
-
+// Arrow function para ver si estamos comiendo mucho o simplemente comiendo en el cuales nosotros indicaremos un valor aleatorio con una condicion de probabilidad que en este caso por dar una idea fue el 30%, numeroAleatorio < 0.3(30%)
+// Por lo general el Math.random() solo nos da valores aleatorios desde 0.00 en adelante por ende al querer solo el 30% del tiempo en el cual nosotros comemos, si es mas en tu caso simplemete le cambias por el que sea por ej: 50% = 0.5
 const comeMucho = () => Math.random() < 0.3
+// Hago 40% de ejercicios estadisticamente por mi, esto puede variar segun que tan precisos queremos ser
 const realizaDeporte = () => Math.random() < 0.4
-
+// Condicion que siempre se ejecutara lo de adentro, mientras(while) el peso que tengo no haya bajado los 3kg que indicamos como nuestra meta ira haciendo lo que establecimos dentro increible verdad :^)
 while (rene.peso > META) {
     
     if(comeMucho()){
@@ -48,8 +52,9 @@ while (rene.peso > META) {
     if(realizaDeporte()){
         decrementoDePeso(rene);
     }
-
+    // aumenta en 1 la cantidad de dias en el cual nos esta tomando bajar los 3kg
     cantidadDeDias++;
 }
-
+// Aqui usamor la interpolacion de ES6, por el cual simplemente usando las `` nosotros podemos usar las ${variable} para no tener que concatenar(o unir) los valores que nosotros queramos imprimir en pantalla o en su defecto como ahora por consola
+// A que es mejor que usar 'Pasaron' + cantidadDeDias + ' dias hasta que ' + 'rene.nombre' + ' adelgazo 3kg' aque si no lo nieges T_T
 console.log(`Pasaron ${cantidadDeDias} dias hasta que ${rene.nombre} adelgazo 3kg`);
