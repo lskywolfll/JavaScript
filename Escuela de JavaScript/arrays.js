79d8c882328045f2cd792b1fea6caf97213b34ee
René <renealejandrosanchezmorales@gmail.com>
2019-08-15 18:54:34 -0400

    Termino de cajas de informaciones en platzi

--- Escuela de JavaScript/arrays.js
+++ Escuela de JavaScript/arrays.js
@@ -2,37 +2,43 @@
 var rene = {
     nombre: 'René',
     apellido: 'Sanchez',
-    altura: 1.75
+    altura: 1.75,
+    libros: 182
 };
 
 var vicky = {
     nombre: 'Vicky',
     apellido: 'Zapata',
-    altura: 1.56
+    altura: 1.56,
+    libros: 91
 };
 
 var paula = {
     nombre: 'Paula',
     apellido: 'Barros',
-    altura: 1.76
+    altura: 1.76,
+    libros: 182
 };
 
 var alan = {
     nombre: 'Alan',
     apellido: 'Perez',
-    altura: 1.86
+    altura: 1.86,
+    libros: 90
 };
 
 var martin = {
     nombre: 'Martin',
     apellido: 'Gomez',
-    altura: 1.85
+    altura: 1.85,
+    libros: 132
 };
 
 var dario = {
     nombre: 'Dario',
     apellido: 'Jaurez',
-    altura: 1.71
+    altura: 1.71,
+    libros: 78
 };
 // Creamos una cajita que contendra todas nuestras personas y las ordenara en base al orden en el cual guardamos los datos de las personas(objetos)
 var personas = [rene,alan,martin,vicky,paula];
@@ -97,3 +103,26 @@ const pasarAlturaACms2 = persona => ({
 var personasCms = personas.map(pasarAlturaACms);
 
 console.log(personasCms);
+
+// Esta forma de obtenerla tambien esta bien pero la funcion siguiente lo hace aun mas sencillo el obtener el dato completo aun que este tampoco esta mal
+// var acumulador = 0;
+// for (let i = 0; i < personas.length; i++) {
+//     acumulador += personas[i].libros;
+//     console.log(acumulador);
+// }
+
+// Aqui creamos la funcion que se encargara de tener la cantidad de libros en total de todas las personas que tienen u leen libros
+// necesitamos 2 valores por ende usaremos un parentecis(contador, persona) => contador += persona.propiedad(en este caso los libros que posee)
+const cantidadDeLibros = (contador, persona) => contador += persona.libros;
+// La otra forma de hacerlo aun mas sencillo es esta
+const cantidadDeLibros2 = (contador, {libros}) => contador += libros;
+
+// Supongamos que nosotros queremos saber el total de un valor en concreto de los datos que tenemos, como ej usaremos que cada persona lee libros o posee libros
+// En este punto ahora solo queremos saber el valor que tiene cada uno y sacar un total de todas las personas que poseen esa propiedad o caracteristica que tambien puede ser que tengan hijos o no, todo depende del caso que nosotros queramos resolver o crear una solucion para tener ese dato total
+// El unico fin que tiene la funcion reduce es obtener un valor total de algun dato u cantidad de cosas que poseemos en total
+// Pongamos que quiero recibir un total de todos los autos que tienen todas las personas que estan en mi cajita de personitas, entonces para esa instancia usaria un reduce en la cajita y creo una funcion que se encargara de obtener esos valores, tambien tengo que indicarle en que posicion de mi cajita empezara a obtener los datos en este punto nosotros usamos el 0
+// iniciara desde el primer dato que se creo en nuestra cajita, asi que primero creamos una funcion que se encarge de proveernos ese valor que nosotros queremos en este punto nosotros necesitamos saber el total de libros de las personas
+// Creamos una funcion que se encarga de tener las cantidades de libros y devolvernos ese resultado para esto creamos una funcion anonima, aparte de nosotros haber creado la funcion tenemos que ahora darle desde que punto nosotros queremos obtener el total en este caso queremos todos empezando desde el inicio (0) final(cajita -1)
+var totalDeLibros = personas.reduce(cantidadDeLibros2, 0);
+
+console.log(totalDeLibros);
