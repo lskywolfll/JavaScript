75d86ced20993d47a4ab0f90234b5b1aa36aa810
René <renealejandrosanchezmorales@gmail.com>
2019-08-26 10:07:42 -0400

    Mejora de mi respuesta en el ejercicio

--- Master de JavaScript/01-ejercicio-Arrays.js
+++ Master de JavaScript/01-ejercicio-Arrays.js
@@ -10,111 +10,104 @@
  */
 
 //  Solucion Mia
-// var limite = 0;
-// var numeros = [];
+var limite = 0;
+var numeros = [];
 
-// for (let index = 0; index < 6; index++) {
-//     let elemento = parseInt(prompt('Introduce tu numero', 0));
-//     numeros.push(elemento);
-//     console.log(elemento);
-// }
+function escribirEnPagina(cajita,ordenamiento = "") {
+    document.write(`Listamiento de los elementos de nuestra cajita ${ordenamiento}`);
+    document.write('<ul>');
+    for (const posicion in cajita) {
+        if (cajita.hasOwnProperty(posicion)) {
+            const element = cajita[posicion];
+            document.write(`<li>Elemento encontrado: ${element}</li>`);
+            // Test
+            console.log(element);
+        }
+    }
+    document.write('</ul>');
+}
 
-// var ordenanzas = ['Sin ordenar', 'Ordenados', 'Alrevez']
+for (let index = 0; index < 6; index++) {
+    let elemento = parseInt(prompt('Introduce tu numero', 0));
+    if(elemento == null || isNaN(elemento)){
+        elemento = parseInt(prompt('Introduce un numero valido', 0));
+    }else{
+        numeros.push(elemento);
+        console.log(elemento);
+    }
+}
 
-// for (let indice = 0; indice < ordenanzas.length; indice++) {
-//     const element = ordenanzas[indice];
-//     if (element == 'Sin ordenar') 
-//     {
-//         document.write(element);
-//         document.write('<ul>');
-//         for (const ubicacion in numeros) {
-//             if (numeros.hasOwnProperty(ubicacion)) {
-//                 const element = numeros[ubicacion];
-//                 document.write(`<li>Elemento dentro de la cajita ${element}</li>`)
-//                 console.log(element);
-//             }
-//         }
-//         document.write('</ul>');
-//     }
-//     else if(element == 'Ordenados')
-//     {
-//         var ordenInverso = numeros.sort();
+var ordenanzas = ['Sin ordenar', 'Ordenados', 'Alrevez']
 
-//         document.write(element);
-//         document.write('<ul>');
-//         for (const ubicacion in numeros) {
-//             if (numeros.hasOwnProperty(ubicacion)) {
-//                 const element = numeros[ubicacion];
-//                 document.write(`<li>Elemento dentro de la cajita ${element}</li>`)
-//                 console.log(element);
-//             }
-//         }
-//         document.write('</ul>');
-//     }
-//     else
-//     {
-//         var ordenBueno = numeros.reverse();
-//         document.write(element);
-//         document.write('<ul>');
-//         for (const ubicacion in numeros) {
-//             if (numeros.hasOwnProperty(ubicacion)) {
-//                 const element = numeros[ubicacion];
-//                 document.write(`<li>Elemento dentro de la cajita ${element}</li>`)
-//                 console.log(element);
-//             }
-//         }
-//         document.write('</ul>');
-//     }
-// }
+for (let indice = 0; indice < ordenanzas.length; indice++) {
+    const opcion = ordenanzas[indice];
+    if(opcion == 'Sin ordenar')
+    {
+        escribirEnPagina(numeros,opcion);
+    }
+    else if(opcion == 'Ordenados')
+    {
+        // Ordena solamente en base al primer digito del numero en este caso y al igual que en letras por orden alfabetico
+        var ordenado = numeros.sort( (primerNumero,segundoNumero) => primerNumero - segundoNumero);
+        escribirEnPagina(ordenado,opcion);
+    }
+    else
+    {
+        var inverso = numeros.reverse();
+        escribirEnPagina(inverso,opcion);
+    }
+}
 
-// document.writeln(`Elementos que contiene nuestra cajita es: ${numeros.length}`)
+document.writeln(`<hr>Elementos que contiene nuestra cajita es: ${numeros.length}`)
 
-// var buscar = parseInt(prompt('Introduce un numero para ver si existe',0));
+var buscar = parseInt(prompt('Introduce un numero para ver si existe',0));
 
-// if(numeros.indexOf(buscar) > -1){
-//     alert(`Tu numero ingresado ${buscar} si existe y esta ubicado en la posicion ${numeros.indexOf(buscar)}`)
-// }else{
-//     alert('Tu numero no existe saquese');
-// }
+var comprobacion = numeros.findIndex( posicion => posicion == buscar);
+
+if(comprobacion > -1){
+    document.write(`<hr>Tu numero ingresado ${buscar} si existe y esta ubicado en la posicion ${numeros.indexOf(buscar)}`);
+}else{
+    document.write('<hr>Tu numero no existe saquese');
+}
 
 // Solucion de victor
 
 // Pedir 6 numeros
-var numeros2 = [];
+// var numeros2 = [];
 
-for (let indice = 0; indice <= 5; indice++) {
-    numeros2.push(parseInt(prompt('Introduce un numero', 0)));
-}
+// for (let indice = 0; indice <= 5; indice++) {
+//     numeros2.push(parseInt(prompt('Introduce un numero', 0)));
+// }
 
-// Mostrar en el cuerpo de la pagina
-function mostrarArray(elementos, textoCustom = "") {
-    document.write(`<h1>Contenido del array ${textoCustom}</h1>`);
-    document.write("<ul>")
-    elementos.forEach((elemento, index) => {
-        document.write(`<li>${elemento}</li>`)
-    });
-    document.write("</ul>")
-}
+// // Mostrar en el cuerpo de la pagina
+// function mostrarArray(elementos, textoCustom = "") {
+//     document.write(`<h1>Contenido del array ${textoCustom}</h1>`);
+//     document.write("<ul>")
+//     elementos.forEach((elemento, index) => {
+//         document.write(`<li>${elemento}</li>`)
+//     });
+//     document.write("</ul>")
+// }
 
-// Ordenar y mostrar
-numeros2.sort( (a,b) => a - b);
+// // Ordenar y mostrar
+// numeros2.sort( (a,b) => a - b);
 
-mostrarArray(numeros2,'Ordenado');
+// mostrarArray(numeros2,'Ordenado');
 
-// Invertir y mostrar
-numeros2.reverse();
-mostrarArray(numeros2,'Revertido');
+// // Invertir y mostrar
+// numeros2.reverse();
+// mostrarArray(numeros2,'Revertido');
 
-//Cuantos elementos tiene un array
-document.write(`Elementos del array: ${numeros2.length} elementos`);
+// //Cuantos elementos tiene un array
+// document.write(`Elementos del array: ${numeros2.length} elementos`);
 
-// Busqueda al usuario una pregunta de que quiere encontrar en una posicion
-var busqueda = parseInt(prompt('Busca un numero',0));
-var buscado = numeros2.findIndex( posicion => posicion == busqueda);
+// // Busqueda al usuario una pregunta de que quiere encontrar en una posicion
+// var busqueda = parseInt(prompt('Busca un numero',0));
+// var buscado = numeros2.findIndex( posicion => posicion == busqueda);
 
-if(buscado && buscado != -1){
-    document.write('<hr><h1>Elemento encontrado!!!</h1>');
-    document.write(`<h1>Posicion de la busqueda: ${buscado}</h1><hr>`);
-}else{
-    document.write(`<h1>Elemento no encontrado</h1>`);
-}
+// if(buscado && buscado != -1){
+//     document.write('<hr><h1>Elemento encontrado!!!</h1>');
+//     document.write(`<h1>Posicion de la busqueda: ${buscado}</h1><hr>`);
+// }else{
+//     document.write(`<h1>Elemento no encontrado</h1>`);
+// }
